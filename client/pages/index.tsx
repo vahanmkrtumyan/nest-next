c892bb31ec31154fb5274e86cbcce5f5c427d5e3
vahanmkrtumyan <35697975+vahanmkrtumyan@users.noreply.github.com>
2020-02-03 17:33:03 +0400

    Added typescript to front

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Nav />
+
+  </div>
+)
+
+export default Home
